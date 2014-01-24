e98c39bdbfda5225a4819f8898f044d2f453a573
Tan Shiaw Uen <shiawuen@gmail.com>
2014-01-24 17:32:15 +0800

    Added animation effects

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+
+$(document).ready(function(){
+    $('#animation').on('click', '.anim-block', function(){
+        var $el = $(this);
+        var cls = $el.text() + ' animated';
+        $el.removeClass(cls)
+            .addClass(cls)
+            .one('webkitAnimationEnd mozAnimationEnd oAnimationEnd animationEnd', function(){
+                $(this).removeClass(cls);
+            });
+    });
+});
